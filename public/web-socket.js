303e9eb2622acba5aa3ce0c4e30f38740bf0e95c
Ben Lesh <blesh@netflix.com>
2015-05-13 15:25:07 -0700

    tweaks

--- public/web-socket.js
+++ public/web-socket.js
@@ -2,20 +2,20 @@ var endpoints = Rx.Observable.just('ws://localhost:3000/talk_people');
 
 // Create some observers so we can see when we open and close the socket.
 var closes = new Rx.Subject();
-closes.subscribe(x => {
+closes.subscribe(() => {
   $('.socket-status').text('closed');
   $('.socket-spinner').hide();
   console.info('socket closed')
 });
 
 var opens = new Rx.Subject();
-opens.subscribe(x => {
+opens.subscribe(e => {
   $('.socket-status').text('open');
   $('.socket-spinner').show();
   console.info('socket opened')
 });
 
-var socket = new RxSocketSubject({ connections: endpoints, closingObserver: closes, openObserver: opens });
+var socket = new RxSocketSubject({ connections: endpoints, openObserver: opens, closingObserver: closes });
 
 var multiplexer = socket.multiplex();
 
@@ -40,26 +40,29 @@ Rx.Observable.merge(thinkersChecked, phoneUsersChecked, legCrossersChecked)
     { type: 'unsub', key: key }, 
     // a filter to select the data for this stream
     request => ((data) => data.key === request.key))
-      // but only take them until the checkbox is unchecked
-      .takeUntil(Rx.Observable.fromEvent($(`input[value="${key}"]`), 'change').take(1))
       // if the stream errors retry when...
-      .retryWhen(errors => {
-        if(window.onLine) {
+      .retryWhen(errors => errors.flatMap(error => {
+        $('.spinner-' + key).hide();
+        if(window.navigator.onLine) {
           // if we're online, try an exponential step back
-          return Rx.Observable.timer(Math.min(30000, Math.pow(2, exponent++) + 100));
+          var delay = Math.min(30000, Math.pow(2, exponent++) + 100);
+          console.warn('retry in ' + delay + ' ms');
+          return Rx.Observable.timer(delay);
         } else {
           // if we're offline, wait for the network to come back
           return Rx.Observable.fromEvent(window, 'online').take(1);
         }
-      })
+      }))
+      // but only take them until the checkbox is unchecked
+      .takeUntil(Rx.Observable.fromEvent($(`input[value="${key}"]`), 'change').take(1))
       // finally remove the spinner when the stream ends
-      .finally(() => (console.log(key), $('.spinner-' + key).hide()))
-      // if we get a successful message, reset our exponent for retry step back
-      .do(() => exponent = 0))
-      // also let's log out successful messages, just because
-      .do(({ key, value }) => console.log('%s: %s', key, value))
-      // show a spinner for the stream
-      .do(({ key }) => $('.spinner-' + key).show())
+      .finally(() => (console.log(key), $('.spinner-' + key).hide())))
+  // if we get a successful message, reset our exponent for retry step back
+  .do(() => exponent = 0)
+  // also let's log out successful messages, just because
+  .do(({ key, value }) => console.log('%s: %s', key, value))
+  // show a spinner for the stream
+  .do(({ key }) => $('.spinner-' + key).show())
   // now subscribe to the whole thing to wire it up
   .subscribe(
     // each successful message, update the DOM
